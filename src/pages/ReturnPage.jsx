7614f2fae71198a07b4ea0f698832f524aa7ca6c
Ramcharan <ramcharxn@gmail.com>
2022-04-06 19:45:15 +0530

    changes on eeturn page

--- src/pages/ReturnPage.jsx
+++ src/pages/ReturnPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 import ReturnMain from './components/ReturnMain'
@@ -6,6 +6,7 @@ import ReturnMain from './components/ReturnMain'
 export default function ReturnPage() {
 
     const [medArray, setMedArray] = useState([])
+    const [userData, setUserData] = useState([])
     const [billNo, setBillNo] = useState('')
     const [searchOn, setSearchOn] = useState(false)
 
@@ -13,13 +14,23 @@ export default function ReturnPage() {
         e.preventDefault()
         axios.post(`http://localhost:5000/return/${billNo}`,{billNo})
         .then(res => (
-            setMedArray(res.data), setSearchOn(true)
+            setMedArray(res.data[0]), setSearchOn(true), setUserData(res.data[1])
+            
             // setMedArray(arr => [res.data]), setSearchOn(true)
         ))
         .catch(err => console.log(err.message))
       }
+
+      const handleClick = () => {
+          console.log('clicked')
+        axios.post('http://localhost:5000/returnMed',{medArray})
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err.message))
+      }
+
       //setMedArray( arr => [...arr, res.data])
-      console.log("Return Page",medArray)
+      console.log("Return Page",medArray.cartItems)
+      console.log('userData',userData)
 
     return (
         <div>
@@ -29,9 +40,9 @@ export default function ReturnPage() {
             </form>
 
         {searchOn ? <ReturnMain medArray={medArray} />:console.log('waiting for input')}
-
-        <h3>Total Amount To Return:</h3>
              
+        <button onClick={handleClick}>return</button>
+
         </div>
     )
 }
--- src/pages/components/ReturnMain.jsx
+++ src/pages/components/ReturnMain.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import ReturnProduct from './ReturnProduct';
 
 export default function ReturnMain(props) {
